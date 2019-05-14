c98c0b3f049e42f2623464b8864471ee8acd4b5e
kalin <kalin.eh@gmail.com>
2019-05-14 00:53:29 -0700

    wip

--- Program.cs
+++ Program.cs
@@ -10,55 +10,257 @@ namespace tonytext
 {
     class Skater
     {
+        private Random random;
+
         public string name;
-        public float height;
+        public int height;
+        public int balance;
+        public bool bail;
         public bool grind;
         public bool manual;
+        public bool grab;
+        public bool kickflip;
         public Area area;
         public int score;
+        public int combo;
         public int multiplier;
 
         public Skater()
         {
+            random = new Random();
+
             name = "Skater";
             area = new Area();
         }
 
+        public void PrintDebug()
+        {
+            Console.WriteLine("SKATER {0} -- ({1} x{2})", name, combo, multiplier);
+            Console.WriteLine("> AREA: {0}", area.current);
+            Console.WriteLine("> score: {0}", score);
+            Console.WriteLine("> height: {0}", height);
+            Console.WriteLine("> balance: {0}", balance);
+            Console.WriteLine("> grind: {0}", grind);
+            Console.WriteLine("> manual: {0}", manual);
+            Console.WriteLine("> grab: {0}", grab);
+            Console.WriteLine("> kickflip: {0}", kickflip);
+        }
+
         public void Print()
         {
-            Console.WriteLine("SKATER {0} - {1} {2} {3} ({4} x{5})", name, height, grind ? "grinding" : "", manual ? "manualing" : "", score, multiplier);
+            var comboText = "";
+            if (combo > 0)
+                comboText = string.Format(" ({0} x{1})", combo, multiplier);
+
+            Console.Write("SKATER {0} - {1} points{2}", name, score, comboText);
+
+            if (score > 0)
+                Console.Write(" - ({0} x{1})", combo, multiplier);
+
+            if (height > 0)
+                Console.Write(" - airborne ({0}m)", height);
+
+            if (bail)
+                Console.Write(" - bailed!");
+            if (grab)
+                Console.Write(" - grabbing!");
+            if (kickflip)
+                Console.Write(" - kickflipping!");
+            if (grind)
+                Console.Write(" - grinding ({0})!", balance);
+            if (manual)
+                Console.Write(" - manualling ({0})!", balance);
+
+            Console.WriteLine("");
         }
 
         public void Act(Action action)
         {
-            Console.WriteLine("SKATER {0} does {0}", action);
+            if (bail)
+            {
+                Console.WriteLine("SKATER {0} recover!", action);
+                bail = false;
+                return;
+            }
+
+            Console.WriteLine("SKATER {0} does {1}", name, action);
+
+            var dir = 0;
+            var landed = false;
+            var tryJump = false;
+            var tryManual = false;
+            var tryGrind = false;
+            var tryGrab = false;
+            var tryKickflip = false;
 
             switch (action)
             {
                 case Action.None:
-                    area.Move(0);
+                    dir = 0;
                     break;
 
                 case Action.Left:
-                    area.Move(1);
+                    dir = 1;
                     break;
 
                 case Action.Right:
-                    area.Move(2);
+                    dir = 2;
                     break;
 
                 case Action.Jump:
-                    area.Move(0);
+                    dir = 0;
+                    tryJump = true;
                     break;
 
                 case Action.Grind:
-                    area.Move(0);
+                    dir = 0;
+                    tryGrind = true;
                     break;
 
                 case Action.Manual:
-                    area.Move(0);
+                    dir = 0;
+                    tryManual = true;
+                    break;
+
+                case Action.Kickflip:
+                    dir = 0;
+                    tryKickflip = true;
+                    break;
+
+                case Action.Grab:
+                    dir = 0;
+                    tryGrab = true;
                     break;
             }
+
+            area.Move(dir);
+
+            if (height == 0 && tryJump)
+            {
+                if (area.current == Surface.Land)
+                    height += 2;
+                if (area.current == Surface.Kicker)
+                    height += 4;
+                if (area.current == Surface.Ramp)
+                    height += 6;
+
+                manual = false;
+                grind = false;
+                balance = 0;
+            }
+
+            if (height > 0)
+            {
+                height -= 1;
+
+                if (height == 0)
+                    landed = true;
+            }
+
+            if (grab && !tryGrab)
+                grab = false;
+
+            if (grind && dir == 1)
+                balance -= 1;
+            if (grind && dir == 2)
+                balance += 1;
+
+            if (manual && dir == 1)
+                balance -= 1;
+            if (manual && dir == 2)
+                balance += 1;
+
+            if (grind)
+            {
+                var waver = random.Next() % 3;
+                if (waver == 1)
+                    balance -= 1;
+                if (waver == 2)
+                    balance += 1;
+            }
+
+            if (grind && balance < -1)
+            {
+                Bail();
+                return;
+            }
+
+            if (grind && balance > +1)
+            {
+                Bail();
+                return;
+            }
+
+            if (kickflip && height > 0)
+                kickflip = false;
+
+            if (grab && height > 0)
+                combo += 10;
+
+            if (height <= 0 && tryGrab)
+            {
+                Bail();
+                return;
+            }
+
+            if (height <= 0 && tryKickflip)
+            {
+                Bail();
+                return;
+            }
+
+            if (height <= 0 && (area.current == Surface.Land || area.current == Surface.Kicker || area.current == Surface.Ramp) && tryManual)
+                manual = true;
+            if (height <= 0 && (area.current == Surface.Rail) && tryGrind)
+                grind = true;
+
+            if (height > 0 && tryKickflip)
+            {
+                kickflip = true;
+                combo += 10;
+                multiplier += 2;
+            }
+
+            if (height > 0 && tryGrab)
+            {
+                grab = true;
+                combo += 10;
+                multiplier += 1;
+            }
+
+            if (landed)
+                Land();
+        }
+
+        public void Land()
+        {
+            Console.WriteLine("SKATER {0} LANDED!");
+
+            score += combo * multiplier;
+
+            if (combo > 0)
+                Console.WriteLine("  *** {0} points ***", combo * multiplier);
+
+            combo = 0;
+            multiplier = 0;
+            bail = false;
+            grind = false;
+            manual = false;
+            grab = false;
+            kickflip = false;
+        }
+
+        public void Bail()
+        {
+            Console.WriteLine("SKATER {0} BAILED!");
+
+            bail = true;
+            combo = 0;
+            multiplier = 0;
+            grind = false;
+            manual = false;
+            grab = false;
+            kickflip = false;
         }
     }
 
@@ -66,7 +268,6 @@ namespace tonytext
     {
         None,
         Air,
-        Water,
         Land,
         Rail,
         Kicker,
@@ -81,6 +282,8 @@ namespace tonytext
         Jump,
         Grind,
         Manual,
+        Kickflip,
+        Grab,
 
         // 
         //   _0_
@@ -93,7 +296,7 @@ namespace tonytext
 
     public class Area
     {
-        public Random random;
+        private Random random;
 
         public Surface current;
         public Surface ahead;
@@ -103,6 +306,7 @@ namespace tonytext
         public Area()
         {
             random = new Random();
+
             current = Surface.Land;
             ahead = RandomSurface();
             left = RandomSurface();
@@ -113,14 +317,13 @@ namespace tonytext
         {
             var choice = random.Next();
 
-            switch (choice % 6)
+            switch (choice % 5)
             {
                 case 0: return Surface.Air;
-                case 1: return Surface.Water;
-                case 2: return Surface.Land;
-                case 3: return Surface.Rail;
-                case 4: return Surface.Kicker;
-                case 5: return Surface.Ramp;
+                case 1: return Surface.Land;
+                case 2: return Surface.Rail;
+                case 3: return Surface.Kicker;
+                case 4: return Surface.Ramp;
             }
 
             return Surface.None;
@@ -171,14 +374,24 @@ namespace tonytext
         {
             Console.WriteLine("SKATE TIME");
 
+            Console.WriteLine("");
+            Console.WriteLine("   _0_    ");
+            Console.WriteLine("    |     ");
+            Console.WriteLine("   / 7    ");
+            Console.WriteLine(" -------  ");
+            Console.WriteLine("  o   o   ");
+            Console.WriteLine("");
+
             var skater = new Skater();
 
             while (true)
             {
+                Console.WriteLine("");
+
                 skater.Print();
                 skater.area.Print();
 
-                Console.WriteLine("Choose: [W] forward, [A] left, [D] right, [J] jump, [G] grind, [M] manual [ESC] quit");
+                Console.WriteLine("Choose: [W] forward, [A] left, [D] right, [J] jump, [R] grind, [M] manual [G] grab [K] kickflip [ESC] quit");
 
                 var key = Console.ReadKey(true);
                 var action = Action.None;
@@ -191,8 +404,10 @@ namespace tonytext
                     case ConsoleKey.A: action = Action.Left; break;
                     case ConsoleKey.D: action = Action.Right; break;
                     case ConsoleKey.J: action = Action.Jump; break;
-                    case ConsoleKey.G: action = Action.Grind; break;
+                    case ConsoleKey.R: action = Action.Grind; break;
                     case ConsoleKey.M: action = Action.Manual; break;
+                    case ConsoleKey.K: action = Action.Kickflip; break;
+                    case ConsoleKey.G: action = Action.Grab; break;
                 }
 
                 if (exit)
